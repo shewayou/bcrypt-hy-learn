aebe4fb294ff1755253d3fd13b17066d9671a13d
shewayou <jal8113@yahoo.com>
2024-07-15 19:54:08 -0400

    bcrypt 3 run

--- server.js
+++ server.js
@@ -11,19 +11,22 @@ const someOtherPlaintextPassword = 'pass123';
 
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
-
+/* ***
 bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
     console.log(hash);
     bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
       console.log(res);
     });
   });
-
+*** */
 //END_ASYNC
 
 //START_SYNC
 
-
+var hash= bcrypt.hashSync(myPlaintextPassword, saltRounds);
+var result = bcrypt.compareSync(myPlaintextPassword, hash);
+console.log( hash )
+console.log( result )
 
 //END_SYNC
 
